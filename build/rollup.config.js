d8a59756adecca62e2d0fd7ac5073e01f10e6507
Leioy <13902303081@139.com>
2020-12-08 22:03:58 +0800

    chore(rollup.config): modify config

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,19 +3,17 @@ import Vue from 'rollup-plugin-vue'
 import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import css from 'rollup-plugin-css-only'
 
 export default {
 	input: path.resolve(__dirname, '../packages/index.ts'),
 	output: {
-		file: 'dist/dolphin.esm.js',
+		file: 'lib/index.esm.js',
 		format: 'es',
 		name: 'Dolphin',
 	},
 	plugins: [
 		terser(),
 		nodeResolve(),
-		css({output: 'dist/style.css'}),
 		Vue({
 			target: 'browser',
 			css: false,
@@ -25,7 +23,7 @@ export default {
 			tsconfig: path.resolve(__dirname, '../tsconfig.json'),
 			tsconfigOverride: {
 				'include': ['packages/**/*', 'typings/vue-shim.d.ts'],
-				'exclude': ['node_modules', 'packages/__test__'],
+				'exclude': ['node_modules', 'packages/**/__test__/*'],
 			},
 		}),
 	],
