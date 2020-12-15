80b1dea182fb4d23a9264925f2508a0d0c84d428
Leioy <Leioy9312@gmail.com>
2020-12-15 16:53:09 +0800

    chore(gulp): fix gulpfile

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -3,7 +3,7 @@ const less = require('gulp-less')
 const autoprefixer = require('gulp-autoprefixer')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
-const noPrefixFile = /index/
+const noPrefixFile = /(index|base)/
 
 function compile () {
 	return src('../packages/styles/*.less')
@@ -19,7 +19,7 @@ function compile () {
 }
 
 function copyfont () {
-	return src('../packages/styles/common/fonts/**')
+	return src('../packages/styles/fonts/**')
 		.pipe(cssmin())
 		.pipe(dest('../lib/styles/fonts'))
 }
